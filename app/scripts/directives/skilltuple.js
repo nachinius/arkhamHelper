8331586faae58a5056559e50ff8f274dba7527c8
nachinius <ignacio@streamlinesocial.com>
2014-10-01 23:34:54 -0300

    Add skills buttons

---
+++ app/scripts/directives/skilltuple.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name arkhamHelperApp.directive:skillTuple
+ * @description
+ * # skillTuple
+ */
+angular.module('arkhamHelperApp')
+  .directive('skillTuple', function () {
+    return {
+      templateUrl : 'views/templates/skilltupleDirective.html',
+      restrict: 'E',
+      scope: {
+        first: '@',
+        second: '@',
+        inv: '=',
+        high: '@'
+      },
+      link: function postLink(scope, element, attrs) {
+        scope.selected = -1;
+        scope.change = function(val) {
+          scope.selected = val;
+        }
+      }
+    };
+  });
