2bc0d5e8977a63c14779a6b8b592a00734fc18f9
nachinius <ignacio@streamlinesocial.com>
2014-10-07 21:59:13 -0300

    add all common items names and image filename

--- app/scripts/services/common_items_data.js
+++ app/scripts/services/common_items_data.js
@@ -3,30 +3,146 @@
 /**
  * @ngdoc service
  * @name arkhamHelperApp.commonItemsData
- * @description
+ * @description Provides a list of
+ * all common items cards with their info
+ * and their quantity
  * # commonItemsData
  * Constant in the arkhamHelperApp.
  */
 angular.module('arkhamHelperApp').constant('commonItemsData', [
-  {
-    name : '.18 Derringer',
-    price : 3,
-    hands : 1,
-    image : '',
-    quantity : 2
-  },
-  {
-    name : '.38 revolver',
-    price : 4,
-    hands : 1,
-    image : '',
-    quantity : 2
-  },
-  {
-    name : '.45 automatic',
-    price : 5,
-    hands : 1,
-    image : '',
-    quantity : 2
-  }
+    {
+      name : '.18 Derringer',
+      price : 3,
+      hands : 1,
+      image : '18Derringer.png',
+      quantity : 2
+    }, {
+      name : '.38 revolver',
+      price : 4,
+      hands : 1,
+      image : '38Revolver.png',
+      quantity : 2
+    }, {
+      name : '.45 automatic',
+      price : 5,
+      hands : 1,
+      image : '45Automatic.png',
+      quantity : 2
+    }, {
+      name : 'Bullwhip',
+      price : 5,
+      hands : 1,
+      image : 'Bullwhip.png',
+      quantity : 2
+    }, {
+      name : 'Dynamite',
+      price : 5,
+      hands : 1,
+      image : 'Dynamite.png',
+      quantity : 2
+    }, {
+      name : 'MapOfArkham',
+      price : 5,
+      hands : 1,
+      image : 'MapOfArkham.png',
+      quantity : 2
+    }, {
+      name : 'Shotgun',
+      price : 5,
+      hands : 1,
+      image : 'Shotgun.png',
+      quantity : 2
+    }, {
+      name : 'CavalrySaber',
+      price : 5,
+      hands : 1,
+      image : 'CavalrySaber.png',
+      quantity : 2
+    }, {
+      name : 'Food',
+      price : 5,
+      hands : 1,
+      image : 'Food.png',
+      quantity : 2
+    }
+
+    , {
+      name : 'Motorcycle',
+      price : 5,
+      hands : 1,
+      image : 'Motorcycle.png',
+      quantity : 2
+    }, {
+      name : 'TommyGun',
+      price : 5,
+      hands : 1,
+      image : 'TommyGun.png',
+      quantity : 2
+    }, {
+      name : 'Knife',
+      price : 5,
+      hands : 1,
+      image : 'Knife.png',
+      quantity : 2
+    }, {
+      name : 'OldJournal',
+      price : 5,
+      hands : 1,
+      image : 'OldJournal.png',
+      quantity : 2
+    }, {
+      name : 'Whiskey',
+      price : 5,
+      hands : 1,
+      image : 'Whiskey.png',
+      quantity : 2
+    }, {
+      name : 'AncientTome',
+      price : 5,
+      hands : 1,
+      image : 'AncientTome.png',
+      quantity : 2
+    }, {
+      name : 'Cross',
+      price : 5,
+      hands : 1,
+      image : 'Cross.png',
+      quantity : 2
+    }, {
+      name : 'Lantern',
+      price : 5,
+      hands : 1,
+      image : 'Lantern.png',
+      quantity : 2
+    }, {
+      name : 'ResearchMaterials',
+      price : 5,
+      hands : 1,
+      image : 'ResearchMaterials.png',
+      quantity : 2
+    }, {
+      name : 'Axe',
+      price : 5,
+      hands : 1,
+      image : 'Axe.png',
+      quantity : 2
+    }, {
+      name : 'DarkCloak',
+      price : 5,
+      hands : 1,
+      image : 'DarkCloak.png',
+      quantity : 2
+    }, {
+      name : 'LuckyCigaretteCase',
+      price : 5,
+      hands : 1,
+      image : 'LuckyCigaretteCase.png',
+      quantity : 2
+    }, {
+      name : 'Rifle',
+      price : 5,
+      hands : 1,
+      image : 'Rifle.png',
+      quantity : 2
+    }
 ]);
