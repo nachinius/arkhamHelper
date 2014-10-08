f5ea66da955a7fc64b11501770b110a1cb77dc14
nachinius <ignacio@streamlinesocial.com>
2014-10-08 10:19:59 -0300

    Issue #4 add card pile basics

--- app/scripts/services/card_pile.js
+++ app/scripts/services/card_pile.js
@@ -7,17 +7,26 @@
  * # cardPile
  * Factory in the arkhamHelperApp.
  */
-angular.module('arkhamHelperApp')
-  .factory('cardPile', function () {
-    // Service logic
-    // ...
-
-    var meaningOfLife = 42;
-
-    // Public API here
-    return {
-      someMethod: function () {
-        return meaningOfLife;
+angular.module('arkhamHelperApp').factory('cardPile', function() {
+  // Service logic
+  // ...
+  
+  var list = [];
+  
+  // Public API here
+  return {
+    list : list,
+    add : function(ele) {
+      list.push(ele);
+    },
+    removeByName : function(name) {
+      var found = _.find(list, function(card) {
+        return card.name == name;
+      });
+      if (found) {
+        list.splice(_.indexOf(list, found), 1);
+        return true;
       }
-    };
-  });
+    }
+  };
+});
