2aa014420b8896d6f2d151806333d766bae240a4
nachinius <ignacio.peixoto@gmail.com>
2014-11-20 13:34:15 -0300

    create route

---
+++ app/scripts/controllers/arkham_encounters.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name arkhamHelperApp.controller:ArkhamEncountersCtrl
+ * @description
+ * # ArkhamEncountersCtrl
+ * Controller of the arkhamHelperApp
+ */
+angular.module('arkhamHelperApp')
+  .controller('ArkhamEncountersCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
