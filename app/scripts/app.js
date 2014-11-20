8961011e47f8b982cafa1e32f6983c0278bf9e05
nachinius <ignacio.peixoto@gmail.com>
2014-11-20 16:59:36 -0300

    make ga follow deep linking pages

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,42 +8,39 @@
  *
  * Main module of the application.
  */
-angular
-  .module('arkhamHelperApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch',
-    'underscore',
-    'ui.sortable',
-    'Shuffler',
-    'ui.bootstrap'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .when('/investigator', {
-        templateUrl: 'views/investigator.html',
-        controller: 'InvestigatorCtrl'
-      })
-      .when('/settings', {
-        templateUrl: 'views/settings.html',
-        controller: 'SettingsCtrl'
-      })
-      .when('/arkham_encounters', {
-        templateUrl: 'views/arkham_encounters.html',
-        controller: 'ArkhamEncountersCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+angular.module(
+		'arkhamHelperApp',
+		[ 'ngAnimate', 'ngCookies', 'ngResource', 'ngRoute', 'ngSanitize',
+				'ngTouch', 'underscore', 'ui.sortable', 'Shuffler',
+				'ui.bootstrap' ]).config(function($routeProvider) {
+	$routeProvider.when('/', {
+		templateUrl : 'views/main.html',
+		controller : 'MainCtrl'
+	}).when('/about', {
+		templateUrl : 'views/about.html',
+		controller : 'AboutCtrl'
+	}).when('/investigator', {
+		templateUrl : 'views/investigator.html',
+		controller : 'InvestigatorCtrl'
+	}).when('/settings', {
+		templateUrl : 'views/settings.html',
+		controller : 'SettingsCtrl'
+	}).when('/arkham_encounters', {
+		templateUrl : 'views/arkham_encounters.html',
+		controller : 'ArkhamEncountersCtrl'
+	}).otherwise({
+		redirectTo : '/'
+	});
+}).run(
+		[ '$rootScope', '$location', '$window',
+				function($rootScope, $location, $window) {
+					$rootScope.$on('$stateChangeSuccess', function(event) {
+
+						if (!$window.ga)
+							return;
+
+						$window.ga('send', 'pageview', {
+							page : $location.path()
+						});
+					});
+				} ]);
