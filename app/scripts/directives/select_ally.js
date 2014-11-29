8f58de11ca46c83aae8192d3d25c94e6270273c0
nachinius <ignacio.peixoto@gmail.com>
2014-11-29 19:16:32 -0300

    Add selection of allies

---
+++ app/scripts/directives/select_ally.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name arkhamHelperApp.directive:selectAlly
+ * @description # selectAlly
+ */
+angular.module('arkhamHelperApp').directive(
+    'selectAlly',
+    function() {
+
+      return {
+        templateUrl : '/views/templates/selectAllyDirective.html',
+        restrict : 'E',
+        scope: {
+          'inv': '='
+        },
+        controller : function($scope, $element, $attrs, $transclude,
+            cardPileAllies) {
+          $scope.cardPileAllies = cardPileAllies;
+        },
+        link : function postLink($scope, $element, $attrs) {
+          $scope.selecting = function(ally) {
+            console.log($scope);
+            $scope.inv.cards.allies.drawByName(ally.name);
+          }
+        }
+      };
+    });
--- app/scripts/directives/terror_level.js
+++ app/scripts/directives/terror_level.js
@@ -3,19 +3,22 @@
 /**
  * @ngdoc directive
  * @name arkhamHelperApp.directive:terrorLevel
- * @description
- * # terrorLevel
+ * @description # terrorLevel
  */
-angular.module('arkhamHelperApp')
-  .directive('terrorLevel', function (terrorLevelService) {
-    return {
-      templateUrl: '/views/templates/terrorLevel.html', 
-      restrict: 'E',
-      controller: function($scope, $element, $attrs, $transclude, terrorLevelService) {
-    	  $scope.terrorLevel = terrorLevelService.level;
-    	  $scope.increaseTerrorLevel = terrorLevelService.increase;
-      },
-      link: function postLink(scope, element, attrs) {
-      }
-    };
-  });
+angular.module('arkhamHelperApp').directive(
+    'terrorLevel',
+    function() {
+
+      return {
+        templateUrl : '/views/templates/terrorLevel.html',
+        restrict : 'E',
+        controller : function($scope, $element, $attrs, $transclude,
+            terrorLevelService) {
+          $scope.terrorLevel = terrorLevelService.level;
+          $scope.increaseTerrorLevel = terrorLevelService.increase;
+        },
+        link : function postLink(scope, element, attrs) {
+
+        }
+      };
+    });
