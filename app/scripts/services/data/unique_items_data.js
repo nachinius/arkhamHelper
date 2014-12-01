d36af3dbe1f814bc0a9fc3b856e7b98f742464b6
nachinius <ignacio.peixoto@gmail.com>
2014-11-30 21:12:10 -0300

    fix data: there are 4 elder signs

--- app/scripts/services/data/unique_items_data.js
+++ app/scripts/services/data/unique_items_data.js
@@ -73,7 +73,7 @@ angular
             phase: "Any",
             text: "Any Phase: When sealing a gate, lose 1 Stamina and 1 Sanity and return this card to the box. You do not need to make a skill check or spend any Clue tokens to seal the gate. In addition, remove one doom token from the Ancient One's doom track.",
             type: "",
-            quantity: 2
+            quantity: 4
           },
           
           {
